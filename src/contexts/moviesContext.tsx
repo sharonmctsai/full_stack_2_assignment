c0d70855c8e05f6404e8a723c082abfb6c2df1fe
Sharon Tsai <sharonmctsai@gmail.com>
2024-07-07 21:28:43 +0100

    npm install

---
+++ src/contexts/moviesContext.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useCallback } from "react";
+import { BaseMovieProps, Review } from "../types/interfaces";
+
+interface MovieContextInterface {
+    favourites: number[];
+    addToFavourites: ((movie: BaseMovieProps) => void);
+    removeFromFavourites: ((movie: BaseMovieProps) => void);
+    addReview: ((movie: BaseMovieProps, review: Review) => void);  // NEW
+}
+const initialContextState: MovieContextInterface = {
+    favourites: [],
+    addToFavourites: () => {},
+    removeFromFavourites: () => {},
+    addReview: (movie, review) => { movie.id, review},  // NEW
+};
+
+export const MoviesContext = React.createContext<MovieContextInterface>(initialContextState);
+
+const MoviesContextProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+    const [myReviews, setMyReviews] = useState<Review[]>( [] )  // NEW
+
+    const addReview = (movie:BaseMovieProps, review: Review) => {   // NEW
+        setMyReviews( {...myReviews, [movie.id]: review } )
+      };
+
+    const [favourites, setFavourites] = useState<number[]>([]);
+
+    const addToFavourites = useCallback((movie: BaseMovieProps) => {
+        setFavourites((prevFavourites) => {
+            if (!prevFavourites.includes(movie.id)) {
+                return [...prevFavourites, movie.id];
+            }
+            return prevFavourites;
+        });
+    }, []);
+
+    const removeFromFavourites = useCallback((movie: BaseMovieProps) => {
+        setFavourites((prevFavourites) => prevFavourites.filter((mId) => mId !== movie.id));
+    }, []);
+
+    return (
+        <MoviesContext.Provider
+            value={{
+                favourites,
+                addToFavourites,
+                removeFromFavourites,
+                addReview,    // NEW
+            }}
+        >
+            {children}
+        </MoviesContext.Provider>
+    );
+};
+
+export default MoviesContextProvider;
