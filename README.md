8620acb19c2f42c9c26724d234555b0279eeb559
Sharon Tsai <sharonmctsai@gmail.com>
2024-07-31 22:07:12 +0100

    fix dup filter button and filter date before/after

--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 Demonstrate the knowledge of the React framework and ecosystem by designing and developing a SPA
 
 <h4>Features</h4>
--Favorites
--Watchlist
--Upcoming movies
--Most Popular movies
--Most Popular Actors
+<ul>Favorites</ul>
+<ul>Watchlist</ul>
+<ul>Upcoming movies</ul>
+<ul>Most Popular movies</ul>
+<ul>Most Popular Actors</ul>
+<ul>Pagination on Home Page</ul>
 
 List of favorite of movies tagged from TMDB
 Biography and details of popular actors
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,25 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
-import { MoviesContext } from "../contexts/moviesContext";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import FilterMoviesCard from "../components/filterMoviesCard";
+import Fab from "@mui/material/Fab";
+import Drawer from "@mui/material/Drawer";
 
-const HomePage = (props) => {
+const styles = {
+  fab: {
+    marginTop: 8,
+    position: "fixed",
+    top: 2,
+    right: 2,
+  },
+};
+
+const HomePage: React.FC = () => {
   const [page, setPage] = useState(1);
-  
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [titleFilter, setTitleFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+  const [popularityFilter, setPopularityFilter] = useState("0");
+  const [releaseDateBeforeFilter, setReleaseDateBeforeFilter] = useState("");
+  const [releaseDateAfterFilter, setReleaseDateAfterFilter] = useState("");
+
   const { data, error, isLoading, isError } = useQuery(
-      ["discover", { page: page }],
-      getMovies
-    );
+    ["discover", { page }],
+    getMovies
+  );
 
   const handleNext = () => {
-    setPage(old => (old + 1));
+    setPage((old) => old + 1);
   };
 
   const handleBack = () => {
-    setPage(old => Math.max(old - 1, 1));
+    setPage((old) => Math.max(old - 1, 1));
+  };
+
+  const handleChange = (type: string, value: string) => {
+    switch (type) {
+      case "title":
+        setTitleFilter(value);
+        break;
+      case "genre":
+        setGenreFilter(value);
+        break;
+      case "popularity":
+        setPopularityFilter(value);
+        break;
+      case "releaseDateBefore":
+        setReleaseDateBeforeFilter(value);
+        break;
+      case "releaseDateAfter":
+        setReleaseDateAfterFilter(value);
+        break;
+      default:
+        break;
+    }
   };
 
   if (isLoading) {
@@ -31,16 +70,48 @@ const HomePage = (props) => {
 
   const movies = data ? data.results : [];
 
+  const genreId = Number(genreFilter);
+  const displayedMovies = movies
+    .filter((m) => m.title.toLowerCase().includes(titleFilter.toLowerCase()))
+    .filter((m) => (genreId > 0 ? m.genre_ids.includes(genreId) : true))
+    .filter((m) => (popularityFilter ? m.popularity >= Number(popularityFilter) : true))
+    .filter((m) => {
+      const releaseDate = new Date(m.release_date);
+      return releaseDateBeforeFilter ? releaseDate <= new Date(releaseDateBeforeFilter) : true;
+    })
+    .filter((m) => {
+      const releaseDate = new Date(m.release_date);
+      return releaseDateAfterFilter ? releaseDate >= new Date(releaseDateAfterFilter) : true;
+    });
+
   return (
-    <PageTemplate
-      title="Discover Movies"
-      movies={movies}
-      action={(movie) => {
-        return <AddToFavouritesIcon object={movie} targetContext={MoviesContext}/>
-      }}
-      handleNext={handleNext}
-      handleBack={handleBack}
-    />
+    <>
+      <PageTemplate
+        title="Discover Movies"
+        movies={displayedMovies}
+        action={(movie) => <AddToFavouritesIcon {...movie} />}
+        handleNext={handleNext}
+        handleBack={handleBack}
+      />
+      <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        sx={styles.fab}
+      >
+        Filter
+      </Fab>
+      <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <FilterMoviesCard
+          onUserInput={handleChange}
+          titleFilter={titleFilter}
+          genreFilter={genreFilter}
+          releaseDateBeforeFilter={releaseDateBeforeFilter}
+          releaseDateAfterFilter={releaseDateAfterFilter}
+        />
+      </Drawer>
+    </>
   );
 };
+
 export default HomePage;
